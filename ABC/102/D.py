344e0e8170deba79916998f1c7974e01d293f4f7
souko2525 <yusuke.h0808@gmail.com>
2018-08-09 23:16:58 +0900

    fix

--- ABC/102/D.py
+++ ABC/102/D.py
@@ -8,16 +8,12 @@ lft = 0
 rgt = 2
 t = sum_a[-1]
 ans = t
-
 for i in range(1,N-2):
-    while sum_a[i] > sum_a[lft] + sum_a[lft+1]:
+    while sum_a[i] - sum_a[lft+1] > sum_a[lft]:
         lft += 1
 
-    while t + sum_a[i] > sum_a[rgt] + sum_a[rgt+1]:
+    while t - sum_a[rgt+1] > sum_a[rgt] - sum_a[i]:
         rgt += 1
-    print("--------")
-    print(lft)
-    print(rgt)
     p = sum_a[lft]
     q = sum_a[i] - p
     r = sum_a[rgt] - q - p
